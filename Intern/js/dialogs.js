aa862eccc51fa0e7a972b77521f29e4b8f273337
danielslaserart <daniels.laser.art@gmail.com>
2026-09-18 17:36:38 +0200

    Rebuild paint and express surcharges as version 4.7

--- Intern/js/dialogs.js
+++ Intern/js/dialogs.js
@@ -1,4 +1,4 @@
-import { esc } from "./utils.js?v=6.6.40";
+import { esc } from "./utils.js?v=4.7";
 
 const dialog=document.getElementById("appDialog");
 const title=document.getElementById("appDialogTitle");
