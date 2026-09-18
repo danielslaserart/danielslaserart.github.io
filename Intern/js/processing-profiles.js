1f18323f02e300e222d71d1deae556e97c762ca0
danielslaserart <daniels.laser.art@gmail.com>
2026-09-18 17:46:41 +0200

    Add finishing surcharges and release version 6.7

--- Intern/js/processing-profiles.js
+++ Intern/js/processing-profiles.js
@@ -1,6 +1,6 @@
-import { $, esc, num, uid } from "./utils.js?v=4.7";
-import { state, save, normalizeProcessingProfile } from "./storage.js?v=4.7";
-import { appAlert, appConfirm } from "./dialogs.js?v=4.7";
+import { $, esc, num, uid } from "./utils.js?v=6.7";
+import { state, save, normalizeProcessingProfile } from "./storage.js?v=6.7";
+import { appAlert, appConfirm } from "./dialogs.js?v=6.7";
 
 const PROCESS_LABELS={
   vectorEngraving:"Vektorgravur",imageEngraving:"Bildgravur",areaEngraving:"Flächengravur",
