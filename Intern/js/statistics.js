6849c5cc6fe5333de840545003810ceb558c3c41
danielslaserart <daniels.laser.art@gmail.com>
2026-09-18 17:03:36 +0200

    Refresh calculator cache for paint surcharge fix

--- Intern/js/statistics.js
+++ Intern/js/statistics.js
@@ -1,7 +1,7 @@
 import { $, num, euro, esc } from "./utils.js?v=6.6.40";
 import { state, save } from "./storage.js?v=6.6.40";
 import { materialSelections, resolveMaterialSelection } from "./materials.js?v=6.6.40";
-import { rounded, renderCalculator, calculate } from "./calculator.js?v=6.6.40";
+import { rounded, renderCalculator, calculate } from "./calculator.js?v=6.6.41";
 import { setScreen } from "./ui.js?v=6.6.40";
 import { renderMotifEstimator } from "./estimator.js?v=6.6.40";
 import { resetMotifEstimator } from "./estimator.js?v=6.6.40";
