3e4794cede68be2105a12029c7f3222590076d13
danielslaserart <daniels.laser.art@gmail.com>
2026-09-19 12:48:10 +0200

    Add repeat count for area material and release v6.9

--- Intern/js/statistics.js
+++ Intern/js/statistics.js
@@ -1,11 +1,11 @@
-import { $, num, euro, esc } from "./utils.js?v=6.8";
-import { state, save } from "./storage.js?v=6.8";
-import { materialSelections, resolveMaterialSelection } from "./materials.js?v=6.8";
-import { rounded, renderCalculator, calculate } from "./calculator.js?v=6.8";
-import { setScreen } from "./ui.js?v=6.8";
-import { renderMotifEstimator } from "./estimator.js?v=6.8";
-import { resetMotifEstimator } from "./estimator.js?v=6.8";
-import { appAlert, appConfirm } from "./dialogs.js?v=6.8";
+import { $, num, euro, esc } from "./utils.js?v=6.9";
+import { state, save } from "./storage.js?v=6.9";
+import { materialSelections, resolveMaterialSelection } from "./materials.js?v=6.9";
+import { rounded, renderCalculator, calculate } from "./calculator.js?v=6.9";
+import { setScreen } from "./ui.js?v=6.9";
+import { renderMotifEstimator } from "./estimator.js?v=6.9";
+import { resetMotifEstimator } from "./estimator.js?v=6.9";
+import { appAlert, appConfirm } from "./dialogs.js?v=6.9";
 function allMaterialOptions(){
   return `<option value="">Kein Material</option>`+
     materialSelections(null,"main")
