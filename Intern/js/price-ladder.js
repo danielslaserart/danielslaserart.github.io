e2947b23f573e3a503b7c83695863c311071c765
danielslaserart <daniels.laser.art@gmail.com>
2026-09-19 12:47:56 +0200

    Add repeat count for area material and release v6.9

--- Intern/js/price-ladder.js
+++ Intern/js/price-ladder.js
@@ -1,4 +1,4 @@
-import { euro, esc, num } from "./utils.js?v=6.8";
+import { euro, esc, num } from "./utils.js?v=6.9";
 
 const hasValue=value=>value!==null&&value!==undefined&&value!==""&&Number.isFinite(Number(value));
 const firstValue=(...values)=>values.find(hasValue);
