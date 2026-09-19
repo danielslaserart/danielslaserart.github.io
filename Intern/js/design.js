7ae8da5bf855b78f8b0da54248304ad3083816e1
danielslaserart <daniels.laser.art@gmail.com>
2026-09-19 12:47:38 +0200

    Add repeat count for area material and release v6.9

--- Intern/js/design.js
+++ Intern/js/design.js
@@ -1,6 +1,6 @@
-import { $, num, euro, uid } from "./utils.js?v=6.8";
-import { state, save, defaults, getRealProjects } from "./storage.js?v=6.8";
-import { appAlert, appConfirm } from "./dialogs.js?v=6.8";
+import { $, num, euro, uid } from "./utils.js?v=6.9";
+import { state, save, defaults, getRealProjects } from "./storage.js?v=6.9";
+import { appAlert, appConfirm } from "./dialogs.js?v=6.9";
 let editingDesignId=null;
 
 function getDesignDefaults(){
