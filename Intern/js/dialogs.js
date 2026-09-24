7ce460420c056140d2f3b3cca3a1b5becc6c3686
danielslaserart <daniels.laser.art@gmail.com>
2026-09-24 21:45:33 +0200

    Complete V8 module cache update

--- Intern/js/dialogs.js
+++ Intern/js/dialogs.js
@@ -1,4 +1,4 @@
-import { esc } from "./utils.js?v=7.2";
+import { esc } from "./utils.js?v=8.0";
 
 const dialog=document.getElementById("appDialog");
 const title=document.getElementById("appDialogTitle");
