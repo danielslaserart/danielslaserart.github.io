1a44b24c7aca0e4f170d5a3bb68c1454a57085b4
danielslaserart <daniels.laser.art@gmail.com>
2026-09-24 21:33:30 +0200

    Version 8.0: Cache-Busting aktualisieren

--- Intern/js/design.js
+++ Intern/js/design.js
@@ -1,6 +1,6 @@
-import { $, num, euro, uid } from "./utils.js?v=7.2";
-import { state, save, defaults, getRealProjects } from "./storage.js?v=7.2";
-import { appAlert, appConfirm } from "./dialogs.js?v=7.2";
+import { $, num, euro, uid } from "./utils.js?v=8.0";
+import { state, save, defaults, getRealProjects } from "./storage.js?v=8.0";
+import { appAlert, appConfirm } from "./dialogs.js?v=8.0";
 let editingDesignId=null;
 
 function getDesignDefaults(){
