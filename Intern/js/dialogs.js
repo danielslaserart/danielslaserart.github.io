b7b09f654af88ae3658307693df8ef2106515ba0
danielslaserart <daniels.laser.art@gmail.com>
2026-09-24 21:48:50 +0200

    Emergency rollback to stable V7.2

--- Intern/js/dialogs.js
+++ Intern/js/dialogs.js
@@ -1,4 +1,4 @@
-import { esc } from "./utils.js?v=8.0";
+import { esc } from "./utils.js?v=7.2";
 
 const dialog=document.getElementById("appDialog");
 const title=document.getElementById("appDialogTitle");
